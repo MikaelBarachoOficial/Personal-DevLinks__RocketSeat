0f7853fc9b7c2416d59cc9080d62435150ea9a0e
Mikael <kaelbaracho@gmail.com>
2023-07-14 21:31:57 -0300

    otimizando código do light-theme

--- light-mode.js
+++ light-mode.js
@@ -1,29 +1,22 @@
 if (localStorage.getItem('light-mode') === 'on') {
-    lightModeOn()
+    changeTheme()
 }
 
 document.querySelector('#switch-theme').addEventListener('click', () => {
 
-    if (localStorage.getItem('light-mode') === 'on') {
-    
-        lightModeOff()
-
-    } else {
-
-        lightModeOn()
-
-    }
+    changeTheme()
 
 })
 
-function lightModeOn () {
-    localStorage.setItem('light-mode', 'on')
-    document.body.classList.add('light-mode')
-    document.querySelector('.profile img').src = 'assets/Avatar-light.svg'
-}
-
-function lightModeOff () {
-    localStorage.setItem('light-mode', 'off')
-    document.body.classList.remove('light-mode')
-    document.querySelector('.profile img').src = 'assets/Avatar.svg'
+function changeTheme () {
+    
+    if (!document.body.classList.contains("light-mode")) {
+        localStorage.setItem('light-mode', 'on')
+        document.body.classList.add('light-mode')
+       
+    } else {
+        localStorage.setItem('light-mode', 'off')
+        document.body.classList.remove('light-mode')
+        
+    }
 }
